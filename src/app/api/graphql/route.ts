c34f608d7df3168eca6a2cbebdda49b3dda1a0a7
cres <cresellejohn@gmail.com>
2023-07-22 23:19:47 +0800

    Add mongodb connection

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,6 +1,22 @@
-import resolvers from '@/graphql/resolvers'
-import typeDefs from '@/graphql/schema'
+import resolvers from '@/app/api/graphql/resolvers'
+import typeDefs from '@/app/api/graphql/schema'
 import { createSchema, createYoga } from 'graphql-yoga'
+import mongoose from 'mongoose'
+
+// Connect to MongoDB database
+mongoose.connect(
+  process.env.API_MONGODB_URL || 'mongodb://localhost:27017/wallet',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'Connection error:'))
+db.once('open', () => {
+  console.log('🌿 Connected to MongoDB!')
+})
 
 const { handleRequest } = createYoga({
   schema: createSchema({
