a08ee468a1f4c69ed84af5eacf5dd6f7b47c32fb
cres <cresellejohn@gmail.com>
2024-08-31 18:04:43 +0800

    Update environment banner

--- src/components/env-banner/index.tsx
+++ src/components/env-banner/index.tsx
@@ -4,7 +4,8 @@ const EnvironmentBanner = () => {
   if (isProd) return
 
   return (
-    <div className="fixed bottom-0 right-0 bg-color-warning/10 text-color-warning/80 text-center font-medium text-xs py-1 px-2 m-2 rounded-md uppercase">
+    <div className="fixed bottom-0 right-0 bg-color-warning/10 text-color-warning/80 text-center font-medium text-xs py-1 px-2 m-2 rounded-md uppercase flex flex-row gap-1.5 items-center">
+      <div className="w-1.5 h-1.5 bg-color-warning/80 rounded-full" />
       {process.env.NEXT_ENV}
     </div>
   )
