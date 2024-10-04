77985e5fe9d0c6a6e80550e6abb15b0b4f6cbcad
cres <cresellejohn@gmail.com>
2024-10-05 01:04:23 +0800

    Fix svg attributes

--- src/assets/icons/svg/ExportIcon.tsx
+++ src/assets/icons/svg/ExportIcon.tsx
@@ -7,23 +7,23 @@ const ExportIcon: FC<SvgIconProps> = (props) => {
       <path
         d="M13 11L21.2 2.80005"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M22 6.8V2H17.2"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M11 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22H15C20 22 22 20 22 15V13"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </SvgIconBase>
   )
--- src/assets/icons/svg/InfoCircleIcon.tsx
+++ src/assets/icons/svg/InfoCircleIcon.tsx
@@ -7,23 +7,23 @@ const InfoCircleIcon: FC<SvgIconProps> = (props) => {
       <path
         d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M12 8V13"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M11.9945 16H12.0035"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </SvgIconBase>
   )
